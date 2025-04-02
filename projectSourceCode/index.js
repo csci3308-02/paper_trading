1827d9c9f55ea686fd1941f6499fa39718ffd1e3
Grant DeBernardi <grde6719@colorado.edu>
2025-04-02 14:31:06 -0600

    basic index file

---
+++ projectSourceCode/index.js
@@ -0,0 +1,135 @@
+// ----------------------------------   DEPENDENCIES  ----------------------------------------------
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// -------------------------------------  APP CONFIG   ----------------------------------------------
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+// set Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+})
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// -------------------------------------  DB CONFIG AND CONNECT   ---------------------------------------
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+const db = pgp(dbConfig);
+
+// db test
+db.connect()
+  .then(obj => {
+    // Can check the server version here (pg-promise v10.1.0+):
+    console.log('Database connection successful');
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR', error.message || error);
+  });
+
+// -------------------------------------  ROUTES for login.hbs   ----------------------------------------------
+const user = {
+  username: undefined,
+  profits: undefined,
+  moneyHeld: undefined,
+  moneyInStocks: undefined,
+};
+
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+
+// Login submission
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const query = 'select * from users where users.username = $1 LIMIT 1';
+  const values = [email];
+
+  // get the student_id based on the emailid
+  db.one(query, values)
+    .then(data => {
+      user.username = username;
+      user.profits = data.profits;
+      user.moneyHeld = data.moneyHeld;
+      user.moneyInStocks = data.moneyInStocks;
+      req.session.user = user;
+      req.session.save();
+
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect('/login');
+    });
+});
+
+// Authentication middleware.
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  next();
+};
+
+app.use(auth);
+
+// -------------------------------------  ROUTES for home.hbs   ----------------------------------------------
+
+app.get('/', (req, res) => {
+  res.render('pages/home', {
+    username: req.session.user.username,
+    profits: req.session.user.profits,
+    money_held: req.session.user.moneyHeld,
+    money_in_stocks: req.session.moneyInStocks,
+  });
+});
+
+
+
+// -------------------------------------  ROUTES for logout.hbs   ----------------------------------------------
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(function(err) {
+    res.render('pages/logout');
+  });
+});
+
+// -------------------------------------  START THE SERVER   ----------------------------------------------
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
